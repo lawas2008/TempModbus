65551979fb1f8c184c441763649b74e2d6a527af
lawas <996772073@qq.com>
2025-04-21 12:04:25 +0800

    add:布局参数

--- tempwdiget.cpp
+++ tempwdiget.cpp
@@ -1,5 +1,57 @@
 #include "tempwdiget.h"
+#include "QHBoxLayout"
+#include "QVBoxLayout"
+#include "QGroupBox"
 
-TempWdiget::TempWdiget(QWidget *parent)
-    : QWidget{parent}
-{}
+/**
+ * @brief TempWdiget::TempWdiget 主界面
+ * @param parent
+ */
+TempWdiget::TempWdiget(QWidget *parent): QWidget{parent}{
+
+    //温度显示布局
+    QVBoxLayout *tempshowLayout = new QVBoxLayout();
+
+    //温湿度
+    QHBoxLayout *tempCItemLayout = new QHBoxLayout();
+
+    //温度
+    QVBoxLayout *tempItemLayout = new QVBoxLayout();
+    QLabel *tempTile = new QLabel("Temp");
+    tempTile->setStyleSheet("QLabel { font-size: 20px; }");
+    tempLabel = new QLabel("00.0");
+    tempLabel->setStyleSheet("QLabel { color: green; font-size: 30px; }");
+    tempItemLayout->addWidget(tempTile,0,Qt::AlignCenter);
+    tempItemLayout->addWidget(tempLabel,0,Qt::AlignCenter);
+    tempItemLayout->addStretch();
+    tempCItemLayout->addStretch();
+    tempCItemLayout->addLayout(tempItemLayout);
+
+    tempCItemLayout->addSpacing(50);
+
+    //湿度
+    QVBoxLayout *coldItemLayout = new QVBoxLayout();
+    QLabel *coldTile = new QLabel("Cold");
+    coldTile->setStyleSheet("QLabel { font-size: 20px; }");
+    coldLabel = new QLabel("00.0");
+    coldLabel->setStyleSheet("QLabel { color: blue; font-size: 30px; }");
+    coldItemLayout->addWidget(coldTile,0,Qt::AlignCenter);
+    coldItemLayout->addWidget(coldLabel,0,Qt::AlignCenter);
+    coldItemLayout->addStretch();
+    tempCItemLayout->addLayout(coldItemLayout);
+    tempCItemLayout->addStretch();
+
+    //添加温湿度
+    tempshowLayout->addLayout(tempCItemLayout);
+
+    //参数布局
+    QGroupBox *paramGroupBox = new QGroupBox("参数");
+
+    //控制布局
+    QGroupBox *controlGroupBox = new QGroupBox("控制");
+
+    //水平布局居中
+    QHBoxLayout *horizontalContainer = new QHBoxLayout();
+    horizontalContainer->addLayout(tempshowLayout);
+    setLayout(horizontalContainer);
+}
--- tempwdiget.h
+++ tempwdiget.h
@@ -6,6 +6,7 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QComboBox>
+#include <QListView>
 
 class TempWdiget : public QWidget
 {
@@ -17,14 +18,49 @@ private:
     //温度
     QLabel *tempLabel;
     //湿度
-    QLabel *humiLabel;
+    QLabel *coldLabel;
     //端口
     QComboBox *portCombox;
     //波特率打开
     QComboBox *ratesCombox;
     //波特率设置
-    QComboBox *rateComboxSet;
-    //
+    QComboBox *rateSetCombox;
+    //打开串口
+    QPushButton *openSerialBtn;
+    //读取版本
+    QPushButton *getVersionBtn;
+    //所有参数单次读取
+    QPushButton *getParamSingleBtn;
+    //所有参数循环读取
+    QPushButton *getParamCycleBtn;
+    //温湿度单次读取
+    QPushButton *getTempCSingleBtn;
+    //温湿度循环读取
+    QPushButton *getTempCCycleBtn;
+    //温度单次读取
+    QPushButton *getTempSingleBtn;
+    //温度循环读取
+    QPushButton *getTempCycleBtn;
+    //湿度单次读取
+    QPushButton *getColdSingleBtn;
+    //湿度循环读取
+    QPushButton *getColdeCycleBtn;
+    //站号设置
+    QLineEdit *addrLowEdit;
+    QLineEdit *addrHightEdit;
+    QPushButton *addrSetBtn;
+    //波特率设置
+    QPushButton *rateSetBtn;
+    //版本号，站号，波特率显示
+    QLineEdit *showVersionEt;
+    QLineEdit *showAddrEt;
+    QLineEdit *showRateEt;
+    //日志消息
+    QListView *logListView;
+    //发送消息
+    QLineEdit *sendCmdEt;
+    QPushButton *sendCmdBtn;
+
 signals:
 };
 
